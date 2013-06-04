7ef3bf622bd5f683a1b720a72195d362f3d303b9
Felix Longolius <felix_longolius@amored-police.org>
2013-06-04 19:24:08 +0200

    Corrections to test email-functionality
    All systems running without sense.

--- html/sendquestion.php
+++ html/sendquestion.php
@@ -1,15 +1,14 @@
 <?php
-$questionFile = $_GET["id"];
-$questionFilePath = "../content/".$questionfile.".txt";
+$questionFilePath = "../content/".$_GET["id"].".txt";
 
 $name = trim($_POST['name']); //not used
 $email = trim($_POST['email']); //not used
-$subject = "Test-Question " . $_GET["id"]."!";
-$msg = "abc"; //trim(nl2br( file_get_contents($GLOBALS["questionFilePath"]) ) );
+$subject = "Test-Question " . $_GET["id"];
+$msg = file_get_contents($GLOBALS["questionFilePath"]);
 
 //mail settings
 $apperson_address = "testing@amored-police.org";
-$headers = "From: amored-police-Testing\r\n" .
+$headers = "From: no-reply@amored-police.org\r\n" .
     "Content-type:  text/plain; charset=utf-8";
 $message = "Question is:\n\n $msg";
 //send mail
