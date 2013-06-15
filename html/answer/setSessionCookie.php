24d254d13fae97f40d58f2765e2f37f818a7268a
Felix Longolius <felix_longolius@amored-police.org>
2013-06-16 01:26:48 +0200

    Cookie woes

--- html/answer/setSessionCookie.php
+++ html/answer/setSessionCookie.php
@@ -3,7 +3,7 @@ date_default_timezone_set("UTC");
 define('__ROOT__', dirname(dirname(__FILE__))); 
 require_once(__ROOT__.'/../php/answering-system.php'); //a file with etherpad-api-class
 require_once(__ROOT__.'/../php/configuration.php');
-$instance = new EtherpadLiteClient($GLOBALS["etherpadapikey"], $GLOBALS["etherpadcookiehost"].'/api');
+$instance = new EtherpadLiteClient($GLOBALS["etherpadapikey"], $GLOBALS["etherpadcookiehost"]);
 $validUntil = time()+14400; // One day in the future
 $sessionID = $instance->createSession($_GET["groupID"], $_GET["authorID"], $validUntil);
 echo "New Session ID is $sessionID->sessionID\n\n";
