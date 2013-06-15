787e26068153b946909fef268d6299e732d324e6
Felix Longolius <felix_longolius@amored-police.org>
2013-06-16 00:29:50 +0200

    Cookie woes

--- html/answer/setSessionCookie.php
+++ html/answer/setSessionCookie.php
@@ -4,7 +4,7 @@ define('__ROOT__', dirname(dirname(__FILE__)));
 require_once(__ROOT__.'/../php/answering-system.php'); //a file with etherpad-api-class
 require_once(__ROOT__.'/../php/configuration.php');
 $instance = new EtherpadLiteClient($GLOBALS["etherpadapikey"], $GLOBALS["etherpadhost"].'/api');
-$validUntil = mktime(date("H")+3, 0, 0, date("m"), date("d"), date("y")); // One day in the future
+$validUntil = time()+3600; // One day in the future
 $sessionID = $instance->createSession($_GET["groupID"], $_GET["authorID"], $validUntil);
 echo "New Session ID is $sessionID->sessionID\n\n";
 $value = $sessionID->sessionID;
