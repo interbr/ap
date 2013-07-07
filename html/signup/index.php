42763f7cf6ef23371468f1fdb68d387121824770
Felix Longolius <felix_longolius@amored-police.org>
2013-07-07 04:05:58 +0200

    Timezone-chooser

--- html/signup/index.php
+++ html/signup/index.php
@@ -1,31 +1,55 @@
 <?php
+$m12 = date("H:i:s",mktime(gmdate("H")-12));
+$m11 = date("H:i:s",mktime(gmdate("H")-11));
+$m10 = date("H:i:s",mktime(gmdate("H")-10));
+$m9 = date("H:i:s",mktime(gmdate("H")-9));
+$m8 = date("H:i:s",mktime(gmdate("H")-8));
+$m7 = date("H:i:s",mktime(gmdate("H")-7));
+$m6 = date("H:i:s",mktime(gmdate("H")-6));
+$m5 = date("H:i:s",mktime(gmdate("H")-5));
+$m4 = date("H:i:s",mktime(gmdate("H")-4));
+$m3 = date("H:i:s",mktime(gmdate("H")-3));
+$m2 = date("H:i:s",mktime(gmdate("H")-2));
+$m1 = date("H:i:s",mktime(gmdate("H")-1));
+$m0 = date("H:i:s",mktime(gmdate("H")-0));
+$p1 = date("H:i:s",mktime(gmdate("H")+1));
+$p2 = date("H:i:s",mktime(gmdate("H")+2));
+$p3 = date("H:i:s",mktime(gmdate("H")+3));
+$p4 = date("H:i:s",mktime(gmdate("H")+4));
+$p5 = date("H:i:s",mktime(gmdate("H")+5));
+$p6 = date("H:i:s",mktime(gmdate("H")+6));
+$p7 = date("H:i:s",mktime(gmdate("H")+7));
+$p8 = date("H:i:s",mktime(gmdate("H")+8));
+$p9 = date("H:i:s",mktime(gmdate("H")+9));
+$p10 = date("H:i:s",mktime(gmdate("H")+10));
+$p11 = date("H:i:s",mktime(gmdate("H")+11));
 $timezones = 
 array (
   'Please choose' => '',
-  '(GMT-12:00) International Date Line West' => 'Pacific/Wake',
-  '(GMT-11:00) Samoa' => 'Pacific/Apia',
-  '(GMT-10:00) Hawaii' => 'Pacific/Honolulu',
-  '(GMT-09:00) Alaska' => 'America/Anchorage',
-  '(GMT-08:00) Pacific Time (US &amp; Canada); Tijuana' => 'America/Los_Angeles',
-  '(GMT-07:00) La Paz' => 'America/Chihuahua',
-  '(GMT-06:00) Central Time (US &amp; Canada)' => 'America/Chicago',
-  '(GMT-05:00) Bogota' => 'America/Bogota',
-  '(GMT-04:00) Caracas' => 'America/Caracas',
-  '(GMT-03:00) Brasilia' => 'America/Sao_Paulo',
-  '(GMT-02:00) Mid-Atlantic' => 'America/Noronha',
-  '(GMT-01:00) Azores' => 'Atlantic/Azores',
-  '(GMT) London' => 'Europe/London',
-  '(GMT+01:00) Berlin, Lagos' => 'Europe/Berlin',
-  '(GMT+02:00) Istanbul' => 'Europe/Istanbul',
-  '(GMT+03:00) Moscow' => 'Europe/Moscow',
-  '(GMT+04:00) Tbilisi' => 'Asia/Tbilisi',
-  '(GMT+05:00) Karachi' => 'Asia/Karachi',
-  '(GMT+06:00) Novosibirsk' => 'Asia/Novosibirsk',
-  '(GMT+07:00) Bangkok' => 'Asia/Bangkok',
-  '(GMT+08:00) Hong Kong' => 'Asia/Hong_Kong',
-  '(GMT+09:00) Seoul' => 'Asia/Seoul',
-  '(GMT+10:00) Sydney' => 'Australia/Sydney',
-  '(GMT+11:00) New Caledonia' => 'Asia/Magadan',
+  ' '.$m12.'  (GMT-12:00)' => 'Pacific/Wake',
+  ' '.$m11.'  (GMT-11:00)' => 'Pacific/Apia',
+  ' '.$m10.'  (GMT-10:00)' => 'Pacific/Honolulu',
+  ' '.$m9.'  (GMT-09:00)' => 'America/Anchorage',
+  ' '.$m8.'  (GMT-08:00)' => 'America/Los_Angeles',
+  ' '.$m7.'  (GMT-07:00)' => 'America/Chihuahua',
+  ' '.$m6.'  (GMT-06:00)' => 'America/Chicago',
+  ' '.$m5.'  (GMT-05:00)' => 'America/Bogota',
+  ' '.$m4.'  (GMT-04:00)' => 'America/Caracas',
+  ' '.$m3.'  (GMT-03:00)' => 'America/Sao_Paulo',
+  ' '.$m2.'  (GMT-02:00)' => 'America/Noronha',
+  ' '.$m1.'  (GMT-01:00)' => 'Atlantic/Azores',
+  ' '.$m0.'  (GMT)' => 'Europe/London',
+  ' '.$p1.'  (GMT+01:00)' => 'Europe/Berlin',
+  ' '.$p2.'  (GMT+02:00)' => 'Europe/Istanbul',
+  ' '.$p3.'  (GMT+03:00)' => 'Europe/Moscow',
+  ' '.$p4.'  (GMT+04:00)' => 'Asia/Tbilisi',
+  ' '.$p5.'  (GMT+05:00)' => 'Asia/Karachi',
+  ' '.$p6.'  (GMT+06:00)' => 'Asia/Novosibirsk',
+  ' '.$p7.'  (GMT+07:00)' => 'Asia/Bangkok',
+  ' '.$p8.'  (GMT+08:00)' => 'Asia/Hong_Kong',
+  ' '.$p9.'  (GMT+09:00)' => 'Asia/Seoul',
+  ' '.$p10.'  (GMT+10:00)' => 'Australia/Sydney',
+  ' '.$p11.'  (GMT+11:00)' => 'Asia/Magadan',
 );
 define('__ROOT__', dirname(dirname(__FILE__))); 
 require_once(__ROOT__.'/../php/configuration.php'); //a file with configurations
@@ -67,15 +91,13 @@ $getContinents = $dbhandle->query("SELECT * FROM agents_continents");
 	?>
 </ul>
 </div>
-<div class="textdiv">Which Timezone are you in:</div>
+<div class="textdiv">What time is it where you are (Timezone):</div>
 <div class="textdiv">
 	<select name="agenttimezone" class="timezoner" size="1" required >
 		<?php foreach($timezones as $tzone => $tzonesave){
 		echo "<option value=\"".$tzonesave."\">".$tzone."</option>";
 		} ?>
     </select>
-	<br /><br />
-	Current time (GMT): <?php echo gmdate("H:i:s"); ?>
 </div>
 <div class="textdiv">When do you want to receive questions (in your local time):</div>
 <div class="textdiv">
