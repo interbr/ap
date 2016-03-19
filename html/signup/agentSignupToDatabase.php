6b4667f9e6cc4215955e8b435f5fcba68084a1a2
Felix Longolius <felix@weltpolizei.de>
2016-03-19 12:38:50 +0100

    Update agentSignupToDatabase.php

--- html/signup/agentSignupToDatabase.php
+++ html/signup/agentSignupToDatabase.php
@@ -46,7 +46,7 @@ If you want to change your settings (i.e. availability) (activate first):
 At anytime you may pause or delete your account with the following link:
 '.$GLOBALS["aphost"].'/agentstatus/deleteaccount.php?email='.urlencode($agentemail).'&pcode='.$pcode.'&delete=1
 
-For questions regarding this question-answer-system or suggestions, please feel free to write to felix_longolius@amored-police.org';
+For questions regarding this question-answer-system or suggestions, please feel free to write to felix@weltpolizei.de';
 
 if(!$mail->Send()) {
   echo "Ooops, we think there was an error!";
